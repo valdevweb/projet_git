db9fa12e9062978752e86d015916bed727f1b61b
valdevweb <vmo@sfr.fr>
2017-01-15 12:16:13 +0100

    incrementation du compteur

--- page.php
+++ page.php
@@ -1,6 +1,9 @@
 <?php
 $fichier=fopen('compteur.txt','r+');
 $compteur=fgets($fichier);
+$compteur ++;
 echo 'nombre de rafraichissement : ' . $compteur;
+fseek($fichier,0);
+fputs($fichier,$compteur);
 fclose($fichier);
 ?>
